0d920e5a022954ca2b3c2cab417e39dabfbdb50a
Erin Cameron <erincameron11@hotmail.com>
2024-04-05 14:50:24 -0400

    checkboxes added to list items

--- todo-list/src/components/ToDoList.js
+++ todo-list/src/components/ToDoList.js
@@ -5,12 +5,26 @@ import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 export const ToDoList = ({task}) => {
+    // Function for handling the complete click
+    const handleCheck = e => {
+        // If task is complete, add text decoration
+        
+    }
+    
+    // Function for handling the delete button click
+    const handleDelete = e => {
+        
+    }
+
+    
+
     return(
         <div className="ToDoListItem">
+            <input className="checkboxes" type="checkbox" onClick={handleCheck}/>
             <p>{task.task}</p>
             <div>
                 <FontAwesomeIcon className="edit" icon={faPenToSquare} />
-                <FontAwesomeIcon className="delete" icon={faTrash} />
+                <FontAwesomeIcon className="delete" icon={faTrash} onClick={handleDelete}/>
             </div>
         </div>
     );
